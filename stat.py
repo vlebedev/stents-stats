93d336b40a862dc13f2ef1565fa0f7452032be0a
Vladimir Lebedev <valebedev@gmail.com>
2014-02-10 14:40:08 +0400

    first release

--- stat.py
+++ stat.py
@@ -20,12 +20,14 @@ def compute_actual_widths(ax):
 def barh(frame, title='', color='', xlabel='', ylabel='',
          format_str='%s', integer=True, legend_title='',
          figsize=(8, 6), stacked=False, hidevalue=5):
+
+    # plot horizontal bar chart
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111)
     frame.plot(kind='barh', ax=ax, title=title, color=color, stacked=stacked)
 
-    # print values inside bars
-    # stacked bar charts need some more care
+    # print value inside each bar
+    # stacked bars need some more care
     if (stacked):
         actual_widths = compute_actual_widths(ax)
 
@@ -137,7 +139,7 @@ def draw_all():
     mpl.rcParams['font.family'] = 'fantasy'
     mpl.rcParams['font.fantasy'] = 'Arial'
 
-    path = '/Users/wal/work/math/stents-stats/st.csv'
+    path = './st.csv'
     stents = pd.read_csv(path, index_col=1, encoding='UTF-8', sep='\t')
     people = stents.groupby(level=0).first()
 
@@ -232,17 +234,193 @@ def draw_all():
 
     draw_impressions_chart(people)
 
+    corr_task8(stents)
+    print('\n')
+    corr_task13(stents)
+    print('\n')
+    corr_task14(stents)
+    print('\n')
+    corr_task15(stents)
+    print('\n')
+
+
+def corr_task8(stents):
+    # Task 8
+    frame = stents[['hospitalization.diagnosis',
+                    'stents.segmentNo',
+                    'stents.tortuosityDeg',
+                    'stents.calcificationDeg']]
+    # Replace strings with categories
+    frame['hospitalization.diagnosis'] = pd.Categorical.from_array(
+        frame['hospitalization.diagnosis']).labels
+    frame['stents.tortuosityDeg'] = pd.Categorical.from_array(
+        frame['stents.tortuosityDeg']).labels
+    frame['stents.calcificationDeg'] = pd.Categorical.from_array(
+        frame['stents.calcificationDeg']).labels
+    names = {
+        'hospitalization.diagnosis': 'Диагноз',
+        'stents.segmentNo': 'Номер сегмента',
+        'stents.tortuosityDeg': 'Извитость',
+        'stents.calcificationDeg': 'Кальциноз'
+    }
+    frame.rename(columns=names, inplace=True)
+    corr = frame.corr()
+    print('Таблица коэффициентов корреляций к пункту 8 задания\n\n%s' % corr)
+
+
+def corr_task13(stents):
+    # Task 13
+    frame = stents[['stents.stent.len',
+                    'stents.stent.dia',
+                    'stents.segmentNo',
+                    'stents.stenosisType',
+                    'stents.tortuosityDeg',
+                    'stents.calcificationDeg']]
+    # Replace strings with categories
+    frame['stents.stenosisType'] = pd.Categorical.from_array(
+        frame['stents.stenosisType']).labels
+    frame['stents.tortuosityDeg'] = pd.Categorical.from_array(
+        frame['stents.tortuosityDeg']).labels
+    frame['stents.calcificationDeg'] = pd.Categorical.from_array(
+        frame['stents.calcificationDeg']).labels
+    avg_len = frame['stents.stent.len'].mean()
+    avg_dia = frame['stents.stent.dia'].mean()
+    names = {
+        'stents.stent.len': 'Длина стента',
+        'stents.stent.dia': 'Диаметр стента',
+        'stents.stenosisType': 'Тип стеноза',
+        'stents.segmentNo': 'Номер сегмента',
+        'stents.tortuosityDeg': 'Извитость',
+        'stents.calcificationDeg': 'Кальциноз'
+    }
+    frame.rename(columns=names, inplace=True)
+    corr = frame.corr()
+    print('Средняя длина стента:   %.2f мм' % avg_len)
+    print('Средний диаметр стента: %.2f мм\n' % avg_dia)
+    print('Таблица коэффициентов корреляций к пункту 13 задания\n\n%s' % corr)
+
+
+def corr_task14(stents):
+    # Task 14
+    frame = stents[['patient.sex',
+                    'patient.age',
+                    'hospitalization.diagnosis',
+                    'stents.stenosisType',
+                    'stents.segmentNo',
+                    'stents.tortuosityDeg',
+                    'stents.calcificationDeg',
+                    'stents.hResults.success',
+                    'stents.hResults.replaced',
+                    'stents.predilatation',
+                    'stents.postdilatation',
+                    'stents.stent.type']]
+    # Replace strings with categories
+    frame['patient.sex'] = pd.Categorical.from_array(
+        frame['patient.sex']).labels
+    frame['hospitalization.diagnosis'] = pd.Categorical.from_array(
+        frame['hospitalization.diagnosis']).labels
+    frame['stents.stenosisType'] = pd.Categorical.from_array(
+        frame['stents.stenosisType']).labels
+    frame['stents.tortuosityDeg'] = pd.Categorical.from_array(
+        frame['stents.tortuosityDeg']).labels
+    frame['stents.calcificationDeg'] = pd.Categorical.from_array(
+        frame['stents.calcificationDeg']).labels
+    frame['stents.hResults.success'] = pd.Categorical.from_array(
+        frame['stents.hResults.success']).labels
+    frame['stents.hResults.replaced'] = pd.Categorical.from_array(
+        frame['stents.hResults.replaced']).labels
+    frame['stents.predilatation'] = pd.Categorical.from_array(
+        frame['stents.predilatation']).labels
+    frame['stents.postdilatation'] = pd.Categorical.from_array(
+        frame['stents.postdilatation']).labels
+    frame['stents.stent.type'] = pd.Categorical.from_array(
+        frame['stents.stent.type']).labels
+    names = {
+        'patient.sex': 'Пол',
+        'patient.age': 'Возраст',
+        'hospitalization.diagnosis': 'Диагноз',
+        'stents.stenosisType': 'Тип стеноза',
+        'stents.segmentNo': 'Номер сегмента',
+        'stents.tortuosityDeg': 'Извитость',
+        'stents.calcificationDeg': 'Кальциноз',
+        'stents.hResults.success': 'Процедура успешна',
+        'stents.hResults.replaced': 'Замена стента',
+        'stents.predilatation': 'Предилятация',
+        'stents.postdilatation': 'Постдилятация',
+        'stents.stent.type': 'Тип стента'
+    }
+    frame.rename(columns=names, inplace=True)
+    corr = frame.corr()
+    print('Таблица коэффициентов корреляций к пункту 14 задания\n\n%s' % corr)
+
+
+def corr_task15(stents):
+    # Task 15
+    frame = stents[['stents.rResults.problems',
+                    'patient.sex',
+                    'patient.age',
+                    'hospitalization.diagnosis',
+                    'stents.stenosisType',
+                    'stents.segmentNo',
+                    'stents.tortuosityDeg',
+                    'stents.calcificationDeg',
+                    'stents.hResults.success',
+                    'stents.hResults.replaced',
+                    'stents.predilatation',
+                    'stents.postdilatation',
+                    'stents.stent.type']]
+    # Replace strings with categories
+    frame['stents.rResults.problems'] = pd.Categorical.from_array(
+        frame['stents.rResults.problems']).labels
+    frame['patient.sex'] = pd.Categorical.from_array(
+        frame['patient.sex']).labels
+    frame['hospitalization.diagnosis'] = pd.Categorical.from_array(
+        frame['hospitalization.diagnosis']).labels
+    frame['stents.stenosisType'] = pd.Categorical.from_array(
+        frame['stents.stenosisType']).labels
+    frame['stents.tortuosityDeg'] = pd.Categorical.from_array(
+        frame['stents.tortuosityDeg']).labels
+    frame['stents.calcificationDeg'] = pd.Categorical.from_array(
+        frame['stents.calcificationDeg']).labels
+    frame['stents.hResults.success'] = pd.Categorical.from_array(
+        frame['stents.hResults.success']).labels
+    frame['stents.hResults.replaced'] = pd.Categorical.from_array(
+        frame['stents.hResults.replaced']).labels
+    frame['stents.predilatation'] = pd.Categorical.from_array(
+        frame['stents.predilatation']).labels
+    frame['stents.postdilatation'] = pd.Categorical.from_array(
+        frame['stents.postdilatation']).labels
+    frame['stents.stent.type'] = pd.Categorical.from_array(
+        frame['stents.stent.type']).labels
+    names = {
+        'stents.rResults.problems': 'Отд. осложнения',
+        'patient.sex': 'Пол',
+        'patient.age': 'Возраст',
+        'hospitalization.diagnosis': 'Диагноз',
+        'stents.stenosisType': 'Тип стеноза',
+        'stents.segmentNo': 'Номер сегмента',
+        'stents.tortuosityDeg': 'Извитость',
+        'stents.calcificationDeg': 'Кальциноз',
+        'stents.hResults.success': 'Процедура успешна',
+        'stents.hResults.replaced': 'Замена стента',
+        'stents.predilatation': 'Предилятация',
+        'stents.postdilatation': 'Постдилятация',
+        'stents.stent.type': 'Тип стента'
+    }
+    frame.rename(columns=names, inplace=True)
+    corr = frame.corr()
+    print('Таблица коэффициентов корреляций к пункту 15 задания\n\n%s' % corr)
+
 
 def exp():
     mpl.rcParams['font.family'] = 'fantasy'
     mpl.rcParams['font.fantasy'] = 'Arial'
 
-    path = '/Users/wal/work/math/stents-stats/st.csv'
+    path = './st.csv'
 
     stents = pd.read_csv(path, index_col=1, encoding='UTF-8', sep='\t')
-    people = stents.groupby(level=0).first()
+    # people = stents.groupby(level=0).first()
 
-    draw_impressions_chart(people)
 
 draw_all()
 #exp()
